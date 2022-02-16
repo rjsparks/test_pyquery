f3bfcfddbd7f0ea28476f357f2ac7890e495fa2c
Robert Sparks <rjsparks@nostrum.com>
2022-02-16 14:09:55 -0600

    Simple test of pyquery

---
+++ test_pyquery.py
@@ -0,0 +1,32 @@
+from pyquery import PyQuery
+
+def test_pyquery():
+    doc = \
+"""
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>title</title>
+  </head>
+  <body>
+   <table>
+     <tbody>
+      <tr id="too-much-before">
+        <th></th>
+        <td></td>
+      </tr>
+      <tr id="target">
+        <th>Hi There</th>
+        <td></td>
+      </tr>
+      <tr id="too-much-after">
+        <th></th>
+        <td></td>
+    </tbody>
+  </table>
+  </body>
+</html>
+"""
+    q = PyQuery(doc)
+    assert 'Hi There' in q('#target').html()
+    assert 'too-much-after' not in q('#target').html()
